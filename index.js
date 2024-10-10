1ecdfd1f265ae289c7f1731e34ac6dc6a5ca4f68
Eduardo <tkd13000@gmail.com>
2024-10-10 07:16:01 -0600

    It can detect enter key

--- index.js
+++ index.js
@@ -1,44 +1,48 @@
 const button = document.getElementById("mode");
+const input = document.getElementById('input');
 const containerCard = document.querySelector(".containerCard");
 
 
 
 //Create new card that contains country information and flag
-button.addEventListener("click", function()
+input.addEventListener("keydown", function(event)
 {
-    const div = document.createElement("div");
-    const img = document.createElement('img');
-    const div2 = document.createElement('div');
-    const p1 = document.createElement('p');
-    const p2 = document.createElement('p');
-    const p3 = document.createElement('p');
-    const p4 = document.createElement('p');
+    if(event.key === "Enter")
+    {
+        const div = document.createElement("div");
+        const img = document.createElement('img');
+        const div2 = document.createElement('div');
+        const p1 = document.createElement('p');
+        const p2 = document.createElement('p');
+        const p3 = document.createElement('p');
+        const p4 = document.createElement('p');
 
-    // Asigna un id/class al nuevo elemento
-    div.classList.add('card');
-    img.id = "flag";
-    div2.id = 'text';
-    p1.id = "nameCountry";
-    p2.id = "population";
-    p3.id = "region";
-    p4.id = "capital";
+        // Asigna un id/class al nuevo elemento
+        div.classList.add('card');
+        img.id = "flag";
+        div2.id = 'text';
+        p1.id = "nameCountry";
+        p2.id = "population";
+        p3.id = "region";
+        p4.id = "capital";
 
-    //Estilos
-    div.style.marginTop = "50px";
-    div.style.borderRadius = "10px";
+        //Styles
+        div.style.marginTop = "50px";
+        div.style.borderRadius = "10px";
 
-    //add content
-    img.src = "./media/Flag_of_Germany.svg.png";
-    p2.textContent = 'Population:';
-    p3.textContent = 'Region:';
-    p4.textContent = 'Capital:';
+        //add content
+        img.src = "./media/Flag_of_Germany.svg.png";
+        p2.textContent = 'Population:';
+        p3.textContent = 'Region:';
+        p4.textContent = 'Capital:';
 
-    //Append children
-    containerCard.appendChild(div);
-    div.appendChild(img);
-    div.appendChild(div2);
-    div2.appendChild(p1);
-    div2.appendChild(p2);
-    div2.appendChild(p3);
-    div2.appendChild(p4);
+        //Append children
+        containerCard.appendChild(div);
+        div.appendChild(img);
+        div.appendChild(div2);
+        div2.appendChild(p1);
+        div2.appendChild(p2);
+        div2.appendChild(p3);
+        div2.appendChild(p4);
+    }
 })
